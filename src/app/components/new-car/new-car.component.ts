ef7e32a0cf477bb2eb8b89b78ad5fe45c183796c
brent <brent.willems.bw@gmail.com>
2019-01-21 16:26:11 +0100

    fixed styling and back navigation in new-car

--- src/app/components/new-car/new-car.component.ts
+++ src/app/components/new-car/new-car.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Car } from 'src/app/models/car';
 import { CarService } from 'src/app/services/car.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-new-car',
@@ -11,20 +12,20 @@ import { CarService } from 'src/app/services/car.service';
 export class NewCarComponent implements OnInit {
 
   myForm: FormGroup;
-  constructor(private carService: CarService, private formBuilder: FormBuilder) {  }
+  constructor(private carService: CarService, private formBuilder: FormBuilder, private location: Location) {  }
 
   ngOnInit() {
     this.myForm = this.formBuilder.group({
-      manufacturer: [''],
-      model: [''],
-      price: [''],
-      wiki: ['']
+      manufacturer: ['', Validators.required],
+      model: ['', Validators.required],
+      price: ['', Validators.required],
+      wiki: ['/']
     });
   }
 
   onSubmit() {
     const car: Car = <Car>this.myForm.value;
-    this.carService.addCar(car).subscribe();
+    this.carService.addCar(car).subscribe(_ => this.location.back());
   }
 
 }
