2f18f9b72bc5a70c1b4b186292252fcd12d20216
brent <brent.willems.bw@gmail.com>
2019-01-21 16:03:40 +0100

    added back button in edit

--- src/app/components/edit-car/edit-car.component.ts
+++ src/app/components/edit-car/edit-car.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Car } from 'src/app/models/car';
 import { CarService } from 'src/app/services/car.service';
 import { FormGroup } from '@angular/forms';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-edit-car',
@@ -13,15 +14,19 @@ export class EditCarComponent implements OnInit {
   id: number;
   private car: Car;
 
-  constructor(private route: ActivatedRoute, private router: Router, private carService: CarService) { }
+  constructor(private route: ActivatedRoute, private location: Location, private carService: CarService) { }
 
   ngOnInit() {
     this.route.params.subscribe(data => this.id = data.id);
     this.carService.getById(this.id).subscribe(data => this.car = data);
   }
 
+  navigateBack() {
+    this.location.back();
+  }
+
   submit() {
-    this.carService.editCar(this.car).subscribe(() => this.router.navigate(['']));
+    this.carService.editCar(this.car).subscribe(() => this.navigateBack());
   }
 
 }
