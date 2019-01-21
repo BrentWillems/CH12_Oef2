a40c0e1b8a471f7f57d2b3f38cb40754191717d3
brent <brent.willems.bw@gmail.com>
2019-01-21 11:46:57 +0100

    creation car service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarService } from './services/car.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CH12Oef2';
+  cars;
+
+  constructor(private carService: CarService) {}
+
+  ngOnInit(): void {
+  }
 }
